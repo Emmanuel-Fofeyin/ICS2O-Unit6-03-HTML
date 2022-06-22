f9dfa0f7a7c45e92bf00e88c0336f921079bcf6c
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-22 01:41:28 +0000

    finished fix

--- js/script.js
+++ js/script.js
@@ -49,6 +49,6 @@ const getWeather = async (URLAddress) => {
   }
 }
 
-    getWeather(
+getWeather(
   "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
-    )
+)
