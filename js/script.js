ee855fb04edfdfca68491f9d5ad9f524cf859835
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-22 01:23:56 +0000

    it will work

--- js/script.js
+++ js/script.js
@@ -33,23 +33,20 @@ const getWeather = async (URLAddress) => {
     const temperature = jsonData.main.temp - 273.15
     const symbol = jsonData.weather[0].icon
     const description = jsonData.weather[0].description
-    document.getElementById("weather").innerHTML = 
-     "The current weather is " +
-     temperature.toFixed(0) +
-     "°C" +
-     "<img src='https://openweathermap.org/img/wn/" +
-     symbol +
-     "@2x.png' alt='Weather Icon' width='15%'>" +
-     "with " +
-     description
-    
+    document.getElementById("weather").innerHTML =
+      "The current weather is " +
+      temperature.toFixed(0) +
+      "°C" +
+      "<img src='https://openweathermap.org/img/wn/" +
+      symbol +
+      "@2x.png' alt='Weather Icon' width='15%'>" +
+      "with " +
+      description
   } catch (err) {
     console.log(err)
     document.getElementById("weather").innerHTML = 
      "Error fetching current weather."
-  }
+   }
 }
 
-    getWeather(
-      "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
-    )
+    getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
