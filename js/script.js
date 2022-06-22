1a7f4af04819c4623597a045c17cdf6c82dd10a3
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-22 00:23:13 +0000

    15 changed files and folders

--- js/script.js
+++ js/script.js
@@ -1,21 +1,43 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Emmanuel-Fofeyin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emmanuel Fofeyin
+// Created on: Apr 2022
 // This file contains the JS functions for index.html
 
 /**
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/github-page-pwa/sw.js", {
-    scope: "/github-page-pwa/",
+  navigator.serviceWorker.register("/ICS20-Unit6-03-JS/sw.js", {
+    scope: "/ICS20-Unit6-03-JS/",
   })
 }
 
 /**
- * This function displays an alert.
+ * Check servie worker.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS20-Unit6-03-HTML/sw.js", {
+    scope: "/ICS20-Unit6-03-HTML/",
+  })
 }
+
+/**
+ * Get API info.
+*/
+const getWeather = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData.main.temp)
+    const temperature = jsonData.main.temp - 273.15
+    const symbol = jsonData.weather[0].icon
+    const description = jsonData.weather[0].description
+    document.getElementById("weather").innerHTML = "The current weather is " + temperature.toFixed(0) + "°C" + "<img src='https://openweathermap.org/img/wn/" + symbol + "@2x.png' alt='Weather Icon' width='15%'>" + "with " + description
+  } catch (err) {
+    console.log(err)
+    document.getElementById("weather").innerHTML = "Error fetching current weather."
+  }
+}
+
+    getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
